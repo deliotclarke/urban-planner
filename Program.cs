349c88a88187e81bdf7dcb88ffc541c84eb8585f
Daniel Clarke <deliotclarke@gmail.com>
2019-07-10 14:00:58 -0500

    completed exercise - classes: made! plans: planned! - did it

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // how to create classes and and use constructors!!!
+
+            Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            FiveOneTwoEigth.Construct(225.25, 335.75, 33);
+            FiveOneTwoEigth.Purchase("Ted Cruz");
+
+            FiveOneTwoEigth.DisplayBuildInfo();
+
+        }
+    }
+}
